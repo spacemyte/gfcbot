96e603691fc2924bea3397739eb4f5137da72351
Fuseblown <fuseblown@gmail.com>
2025-12-04 15:31:51 -0500

    Update bot status to 'Patrolling for Instagram URLs'

--- bot/main.py
+++ bot/main.py
@@ -66,7 +66,7 @@ async def on_ready():
     await bot.change_presence(
         activity=discord.Activity(
             type=discord.ActivityType.watching,
-            name="for Instagram URLs"
+            name="Patrolling for Instagram URLs"
         )
     )
 
