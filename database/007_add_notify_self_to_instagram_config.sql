100ba4976b27b031d36d5deec6df47e388e7c682
Fuseblown <fuseblown@gmail.com>
2025-12-04 19:03:44 -0500

    Add notify self to instragram config options

--- database/007_add_notify_self_to_instagram_config.sql
+++ database/007_add_notify_self_to_instagram_config.sql
@@ -9,4 +9,5 @@ ADD COLUMN notify_self_replies BOOLEAN DEFAULT false;
 
 -- Add comments
 COMMENT ON COLUMN instagram_embed_config.webhook_reply_notifications IS 'If true, enable webhook reply notifications overall';
+
 COMMENT ON COLUMN instagram_embed_config.notify_self_replies IS 'If true, notify user when they reply to their own webhook-reposted message';
