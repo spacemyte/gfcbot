4af5b5834ef9680b993d843db1d525834ef95c6b
Fuseblown <fuseblown@gmail.com>
2025-12-04 23:51:48 -0500

    Updated bot status message

--- bot/main.py
+++ bot/main.py
@@ -66,7 +66,7 @@ async def on_ready():
     await bot.change_presence(
         activity=discord.Activity(
             type=discord.ActivityType.watching,
-            name="Freeing your social links"
+            name="Currently freeing your Instagram links"
         )
     )
 
